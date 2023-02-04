299666f737d90e4c189d10513722f7499e0f841e
ChilloutGeek <adamgemenez@gmail.com>
2023-02-04 20:13:55 +0800

    add missing signals.py code

--- customer/signals.py
+++ customer/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Profile 
+from .models import CustomerProfile 
 from django.contrib.auth.models import User
 
 @receiver(post_save, sender=User)
 def post_save_create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        CustomerProfile.objects.create(user=instance)

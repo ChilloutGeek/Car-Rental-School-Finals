26e7340b37274fe749f92f1b8808a9407599a924
ChilloutGeek <adamgemenez@gmail.com>
2023-02-23 08:55:53 +0800

    rentacar_v1.0

--- rental/urls.py
+++ rental/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomeRentalView
+from .views import HomeRentalView,RentalCarView
 
 
 urlpatterns = [
-    path('home/', HomeRentalView.as_view(), name="rental")
-   
+    path('home/', HomeRentalView.as_view(), name="rental"),
+    path('rentacar/<str:pk>', RentalCarView.as_view(), name="rentacar")
 ]
--- rental/views.py
+++ rental/views.py
@@ -11,9 +11,16 @@ class HomeRentalView(LoginRequiredMixin, View):
     login_url = 'login'
 
     def get(self, request):
+        
         cars = Car.objects.all()
         form = RentalForm()
         currentcustomer = CustomerProfile.objects.get(user=self.request.user)
+        
+        search_text = request.GET.get('search_text', None)
+
+        if search_text:
+            cars = Car.objects.get(Model__contains=search_text)
+
         return render(request, 'rental/rental.html', {'cars':cars, 'form':form, 'currentcustomer':currentcustomer})
 
     def post(self, request, pk):
@@ -29,7 +36,31 @@ class HomeRentalView(LoginRequiredMixin, View):
             rentalcreate.save()
         return redirect('rental')
 
+class RentalCarView(LoginRequiredMixin, View):
+        
+        login_url = 'login'
 
+        def get(self, request, pk):
+            rental = Rental.objects.get(pk=pk)
+
+            form = RentalForm(instance=rental)
+
+            currentcustomer = CustomerProfile.objects.get(user=self.request.user)
+
+            return render(request, 'rental/rentacar.html', {'rental':rental, 'form':form, 'currentcustomer':currentcustomer})
+        
+        def post(self, request, pk):
+            
+            rental = Rental.objects.get(pk=pk)
+            currentscustomer = CustomerProfile.objects.get(user=self.request.user)
+            form = RentalForm(request.POST, instance=rental)
+
+            if form.is_valid():
+                rentedform = form.save(commit=False)
+                rentedform.Car = Car.objects.get(pk=pk)
+                rentedform.Renter = currentcustomer
+                rentedform.save()
+            return redirect('rental')
 
 
 

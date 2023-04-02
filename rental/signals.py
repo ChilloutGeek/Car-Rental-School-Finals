084a4d9c15ee000b5ebab7d4222d7a94d9413e5c
ChilloutGeek <adamgemenez@gmail.com>
2023-04-02 22:25:39 +0800

    finished credit system

--- rental/signals.py
+++ rental/signals.py
@@ -2,7 +2,11 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Rental
 from customer.models import CustomerProfile
+from django.utils import timezone
+import pytz
 
+ph_tz = pytz.timezone('Asia/Manila')
+now = timezone.now().astimezone(ph_tz)
 
 @receiver(post_save, sender=Rental)
 def increment_car(sender, instance, created, **kwargs):
@@ -11,12 +15,21 @@ def increment_car(sender, instance, created, **kwargs):
         car.Stock_Number -= 1
         car.save()
 
-    if instance.FinishedRent:
+    if instance.FinishedRent and instance.ReturnDate < now :
+        profile = CustomerProfile.objects.get(user=instance.Renter.user)
+        profile.credit_rating -= 5
+        profile.save()
+
+        car = instance.Car
+        car.Stock_Number += 1
+        car.save()
+
+    else:
+
         profile = CustomerProfile.objects.get(user=instance.Renter.user)
         profile.credit_rating += 5
         profile.save()
-    
-    else:
+
         car = instance.Car
         car.Stock_Number += 1
         car.save()
